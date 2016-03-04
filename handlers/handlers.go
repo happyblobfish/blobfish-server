ab4093fc3f0d7fc298574ccab6de217d4daa8a10
Jan <jgrodowski@gmail.com>
2016-03-04 12:26:51 +0100

    Add fileupload to the server

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -33,29 +33,73 @@ func (h *Handler) MemesIndex(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(memesCollection.Memes)
 }
 
-// MemesCreate creates a meme
+// MemesCreate creates a meme from URL
+// Currently it's the only way to support Gifs
+// TODO: function too long
 func (h *Handler) MemesCreate(w http.ResponseWriter, r *http.Request) {
-	meme := &models.Meme{URL: r.FormValue("url")}
-
-	if err := h.db.Update(meme.Save); err != nil {
-		log.Fatal(err)
-	}
-
-	json.NewEncoder(w).Encode(meme)
-}
-
-// MemeDestroy destroys a meme
-func (h *Handler) MemeDestroy(w http.ResponseWriter, r *http.Request) {
-	memeID, err := strconv.ParseUint(mux.Vars(r)["memeID"], 10, 64)
+	meme := &models.Meme{OriginalURL: r.FormValue("url")}
+	resp, err := meme.GetImageBinary()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	meme := &models.Meme{ID: uint64(memeID)}
+	if err := meme.SetData(resp.Body, resp.Header.Get("Content-Type")); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := h.db.Update(meme.Save); err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(meme)
+}
+
+// MemesUpload handles upload of a meme
+func (h *Handler) MemesUpload(w http.ResponseWriter, r *http.Request) {
+	meme := new(models.Meme) // no OriginalUrl ATM
+	// TODO: DRY!!! Lines 76:81 are a great candidate for a Builder struct
+	// REPLY: Yes and no.
+	if err := meme.SetData(r.Body, "image/png"); err != nil { // frontend currently supports only PNGs
+		log.Fatal(err)
+	}
+	if err := h.db.Update(meme.Save); err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(meme)
+}
+
+// MemeGet serves a meme image
+// TODO: return appropriate Content-Type header - may be required?
+func (h *Handler) MemeGet(w http.ResponseWriter, r *http.Request) {
+	meme := h.findMeme(r)
+
+	h.db.View(meme.GetImage)                                             // fetch image
+	w.Header().Set("Content-Length", strconv.Itoa(meme.ContentLength())) // set length
+	w.Header().Set("Content-Type", meme.ContentType)                     // set mime
+	if err := meme.WriteImage(w); err != nil {
+		log.Fatal("error writing binary data")
+	}
+}
+
+// MemeDestroy destroys a meme
+func (h *Handler) MemeDestroy(w http.ResponseWriter, r *http.Request) {
+	meme := h.findMeme(r)
+
 	if err := h.db.Update(meme.Destroy); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func (h *Handler) findMeme(r *http.Request) *models.Meme {
+	memeID, err := strconv.ParseUint(mux.Vars(r)["memeID"], 10, 64)
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	meme := new(models.Meme)
+	h.db.View(models.MemeFetch(meme, memeID))
+	return meme
 }
 
 func openDB() *bolt.DB {
--- models/meme.go
+++ models/meme.go
@@ -2,16 +2,68 @@ package models
 
 import (
 	"encoding/binary"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
 
 	"github.com/boltdb/bolt"
 )
 
 var bucketName = []byte("memes")
+var binBucketName = []byte("memes_bindata")
+var allowedContentTypes = []string{"image/jpeg", "image/png", "image/gif"}
+var maxImageContentLength int64 = 50000000 // 5 Mb
 
 // Meme is a meme
 type Meme struct {
-	URL string `json:"url"`
-	ID  uint64 `json:"id"`
+	URL         string `json:"url"`
+	ID          uint64 `json:"id"`
+	OriginalURL string `json:"original-url"`
+	ContentType string `json:"content-type"`
+	bindata     []byte
+}
+
+// MemeFromBolt creates a meme from bolt's saved data
+func MemeFromBolt(k []byte, v []byte) (*Meme, error) {
+	meme := new(Meme)
+	if err := json.Unmarshal(v, meme); err != nil {
+		return meme, err
+	}
+	meme.ID = binary.BigEndian.Uint64(k)
+	return meme, nil
+}
+
+// MemeFetch fetches all data from memes bucket and assign to meme
+func MemeFetch(memePointer *Meme, id uint64) func(tx *bolt.Tx) error {
+	return func(tx *bolt.Tx) error {
+		meme, err := MemeFromBolt(itob(id), tx.Bucket(bucketName).Get(itob(id)))
+		if err != nil {
+			log.Fatalln(err)
+			return err
+		}
+		// assing new meme's pointer
+		*memePointer = *meme
+		return nil
+	}
+}
+
+// GetImage retrieves binary image for a meme (from memes_bindata bucket) and assign
+// to m.bindata
+func (m *Meme) GetImage(tx *bolt.Tx) error {
+	m.bindata = tx.Bucket(binBucketName).Get(itob(m.ID))
+	return nil
+}
+
+// WriteImage writes binary image to a given io.Writer
+func (m *Meme) WriteImage(w io.Writer) error {
+	if _, err := w.Write(m.bindata); err != nil {
+		return err
+	}
+	return nil
 }
 
 // Save saves meme in bolt storage
@@ -25,11 +77,17 @@ func (m *Meme) Save(tx *bolt.Tx) error {
 	// This returns an error only if the Tx is closed or not writeable.
 	// That can't happen in an Update() call so I ignore the error check.
 	id, _ := bucket.NextSequence()
-	if err := bucket.Put(itob(id), []byte(m.URL)); err != nil {
+	m.ID = id
+	m.URL = fmt.Sprintf("%s/%d", "memes", id)
+	jsonString, err := json.Marshal(m)
+	if err != nil {
 		return err
 	}
-	m.ID = id
-	return nil
+	if err := bucket.Put(itob(id), jsonString); err != nil {
+		return err
+	}
+	// write image binary to memes_bindata bucket
+	return m.writeBindata(tx, id)
 }
 
 // Destroy deletes a meme from the DB
@@ -45,9 +103,55 @@ func (m *Meme) Destroy(tx *bolt.Tx) error {
 	return nil
 }
 
-func (m *Meme) fromBolt(id []byte, url []byte) {
-	m.ID = binary.BigEndian.Uint64(id)
-	m.URL = string(url)
+// SetData takes binary data from reader and saves it to image
+func (m *Meme) SetData(r io.Reader, contentType string) error {
+	m.ContentType = contentType
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	m.bindata = data
+	return nil
+}
+
+// ContentLength returns image's bytes length
+func (m *Meme) ContentLength() int {
+	return len(m.bindata)
+}
+
+// GetImageBinary gets image's binary data from a URL
+func (m *Meme) GetImageBinary() (*http.Response, error) {
+	resp, err := http.Get(m.OriginalURL)
+	if err != nil {
+		errorMsg := fmt.Sprintf("%s %s", "http error", m.OriginalURL)
+		return nil, errors.New(errorMsg)
+	}
+	if resp.StatusCode != 200 || resp.ContentLength > maxImageContentLength {
+		errorMsg := fmt.Sprintf("%s %s", "error status or response too big", resp.StatusCode)
+		return nil, errors.New(errorMsg)
+	}
+	contentTypeValid := false
+	for _, contentType := range allowedContentTypes {
+		if contentType == resp.Header.Get("Content-Type") {
+			contentTypeValid = true
+			break
+		}
+	}
+	if !contentTypeValid {
+		return nil, errors.New("invalid content type header")
+	}
+	return resp, nil
+}
+
+func (m *Meme) writeBindata(tx *bolt.Tx, id uint64) error {
+	bucket, err := tx.CreateBucketIfNotExists(binBucketName)
+	if err != nil {
+		return err
+	}
+	if err := bucket.Put(itob(id), m.bindata); err != nil {
+		return err
+	}
+	return nil
 }
 
 // itob returns an 8-byte big endian representation of v.
